49a6223c59e868e3a948dc6725bb9121c3ac253b
Dongjun Shin <tlsehdwns239@gmail.com>
2023-11-15 18:10:51 +0900

    feat(OutputView): 출력 객체 정의

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,35 @@
+package christmas.view;
+
+import java.io.PrintStream;
+import java.text.DecimalFormat;
+import java.util.Map;
+
+import christmas.event.GivenEvent;
+import christmas.order.Orders;
+import lib.enumeration.Badge;
+import lib.enumeration.Menu;
+
+public class OutputView {
+    private final PrintStream printStream = System.out;
+    private final String NO_ITEM = "없음";
+    private final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###,###");
+    
+    public void viewEventPlanner(Orders orders) {
+        viewOrders(orders);
+        viewTotalPrice(orders);
+        viewGivenBenefit(orders);
+        viewBenefits(orders);
+        viewTotalBenefit(orders);
+        viewPayPrice(orders);
+        viewBadge(orders);
+    }
+
+    public void viewIntro() {
+        printStream.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void viewStartMessege() {
+        printStream.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        printStream.println("");
+    }
+}
